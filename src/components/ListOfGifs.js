0261128e0a6e268c2888f7dd2da25f4b7eea5751
hellmuth trejo <hellmuthtrejo@gmail.com>
2022-06-21 18:32:22 -0400

    loading

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -5,14 +5,22 @@ import getGifs from '../services/getGifs';
 export default function ListOfGifs ({params}) { 
     const {keyword} = params;
     const [gifs, setGifs] = useState([]);
+    const [loading, setLoading] = useState(false);
 
   // useEffects, permite ejecutar funciones cada vez que se renderice el componente. 
   // el segundo argumento es un array, lista de dependencias.
   // el useEffect es equivalente al componentDimount
   // ya que la dependencia es keyword si cambia se vuelve a renderizar el componente.
   useEffect(function() {
-    getGifs({ keyword }).then(resp => setGifs(resp));
+    setLoading(true);
+    getGifs({ keyword }).then(resp => {
+      setGifs(resp);
+      setLoading(false);
+    });
   }, [keyword]);
+
+  if(loading) 
+  return <i>Cargando @</i>
 
     return (<>
       {
