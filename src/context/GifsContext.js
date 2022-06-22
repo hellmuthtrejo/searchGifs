7757183be1fc067280b6ffa83ee43d9eaefc1fef
hellmuth trejo <hellmuthtrejo@gmail.com>
2022-06-22 14:48:06 -0400

    avances

--- src/context/GifsContext.js
+++ src/context/GifsContext.js
@@ -5,8 +5,7 @@ const Context = React.createContext({});
 
 export function GifsContextProvider ({children}){
     const [gifs, setGifs] = useState([]);
-
-    console.log(gifs);
+    
     return <Context.Provider value={{gifs, setGifs}}>
         {children}
     </Context.Provider>
--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,15 +1,15 @@
 import React, {useContext} from 'react'
 // import StaticContext from '../../context/StaticContext';
 import Context  from '../../context/GifsContext';
+import Gif from '../../components/Gif';
 
 export default function Detail ({params}) {
     // const staticConfig = useContext(StaticContext);
     // console.log(staticConfig)
 
-    const context = useContext(Context);
-    console.log(context)
+    const {gifs} = useContext(Context);
     
-  return (
-    <div>Detail of gifs {params.id}</div>
-  )
+    const gif = gifs.find(singleGifs => singleGifs.id === params.id);
+    console.log(gif);
+  return <Gif {...gif} />
 }
